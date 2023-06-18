5ae9664d43f65c400c02161b69c4d320a1c05f52
user.Powerpoffgirl <email.jyotisingh13@gmail.com>
2023-06-18 13:44:46 +0530

    bcrypt changed

--- server/contollers/auth.js
+++ server/contollers/auth.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
@@ -7,8 +7,8 @@ export const register = async (req, res) => {
   try {
     const { firstName, lastName, email, password, mobile } = req.body;
 
-    const salt = await bcrypt.genSalt();
-    const passwordHash = await bcrypt.hash(password, salt);
+    const salt = await bcryptjs.genSalt();
+    const passwordHash = await bcryptjs.hash(password, salt);
 
     const newUser = new User({
       firstName,
@@ -31,7 +31,7 @@ export const login = async (req, res) => {
     const user = await User.findOne({ email: email });
     if (!user) return res.status(400).json({ msg: "User does not exist. " });
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcryptjs.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials. " });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
